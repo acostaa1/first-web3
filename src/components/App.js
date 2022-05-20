faffbaa560a8c0fcba418b55771cfe61e52a1ea1
Angel Acosta <angel.s.acosta1@gmail.com>
2022-05-19 22:16:00 -0400

    added Ether & Gas prices in USD

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Web3 from "web3";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
@@ -13,14 +14,31 @@ class App extends Component {
     };
   }
   async componentDidMount() {
+    //this is my connection to a node on the ethereum blockchain
     const url = "https://mainnet.infura.io/v3/dea555b34aa54edcacb263a506a82880";
     const web3 = new Web3(url);
 
     //latest block on the chain
     const latestBlock = await web3.eth.getBlock("latest");
 
-    //current gas price
+    //current gas price in wei denomination
     const gasPrice = await web3.eth.getGasPrice();
+
+    //current gas price in ether denomination
+    const gasPriceEther = await web3.eth.utils.fromWei(
+      gasPrice.toString(),
+      "ether"
+    );
+
+    //current price of ether in USD
+    const currentEtherUSD = await (
+      await axios.get(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD"
+      )
+    ).data.ETH.USD;
+
+    //current gas price in USD
+    const finalGasUSD = (await currentEtherUSD) * Number(gasPriceEther) * 100;
 
     //latest 10 blocks
     const latestBlocks = [];
@@ -31,21 +49,25 @@ class App extends Component {
     this.setState({
       blockNumber: latestBlock.number,
       difficulty: latestBlock.difficulty,
-      gasPrice: gasPrice,
+      gasPrice: finalGasUSD,
       latestBlocks: latestBlocks,
+      etherPrice: currentEtherUSD,
     });
-
-    console.log(latestBlock);
-    console.log(latestBlocks);
   }
 
   render() {
-    const { blockNumber, difficulty, gasPrice, latestBlocks } = this.state;
+    const {
+      blockNumber,
+      difficulty,
+      gasPrice,
+      latestBlocks,
+      etherPrice,
+    } = this.state;
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <div className="navbar-brand col-sm-3 col-md-2 mr-0">
-            Live Etherium Blockchain Data
+            Live Ethereum Blockchain Data
           </div>
         </nav>
         <div className="container-fluid mt-5">
@@ -53,9 +75,15 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div
                 className="content mr-auto ml-auto"
-                style={{ width: "700px" }}
-              >
-                <h5>Etherium Blockchain Explorer</h5>
+                style={{ width: "700px" }} >
+            
+                <div className="row">
+                  <div className="col-12 mt-4">
+                    {" "}
+                    <h3>Current Ether Price: ${etherPrice}</h3>
+                  </div>
+                </div>
+
                 <div className="row">
                   <div className="col-4 ">
                     <div className="bg-light pt-4 pb-3 m-1">
@@ -73,7 +101,7 @@ class App extends Component {
 
                   <div className="col-4 ">
                     <div className="bg-light pt-4 pb-3 m-1">
-                      <h5>Gas Price</h5>
+                      <h5>Gas Price (USD)</h5>
                       <p>{gasPrice} </p>
                     </div>
                   </div>
